9882f769ac819188cf438c42c893492dbb9af689
MironovED <e.d.mironov@bk.ru>
2022-11-23 21:03:51 +0300

    added parallel command

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,12 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
 	e2e: {
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
 		},
-		baseUrl: "http://qamid.tmweb.ru",
-		specPattern: "cypress/integration/**/*.cy.{js,jsx,ts,tsx}",
+		baseUrl: 'http://qamid.tmweb.ru',
+		specPattern: 'cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
 	},
-	projectId: "k3xayt",
+	projectId: 'k3xayt',
 });
